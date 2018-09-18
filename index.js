5cff13adc1cfdbd0ba8d6cf6842635766af68648
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-09-18 09:22:08 +0200

    log errors to the console

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const getStdin = require('get-stdin');
 const help = require('./lib/help');
 const report = require('./lib/report');
  
-getStdin().then(help).then(report).then(console.log);
+getStdin().then(help).then(report).then(console.log).catch(console.error);
