faaf77477c2eb721b5d38036127636f292468d3d
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-09-18 09:34:21 +0200

    only include review actions

--- test.js
+++ test.js
@@ -20,6 +20,12 @@ const advisories = {
     recommendation: 'upgrade baddep now please',
     severity: 'high',
   },
+  999: {
+    id: 999,
+    overview: 'baddep4000 can DOS you',
+    recommendation: 'upgrade baddep now please',
+    severity: 'high',
+  },
 };
 
 test('should filter output by severity', (t) => {
@@ -57,5 +63,56 @@ test('should filter output by severity', (t) => {
   const result = help(input);
   t.equal(result.actions.length, 1);
   t.equal(result.actions[0].resolves[0].id, 123);
+  t.end();
+});
+
+test('should only include actions for review', (t) => {
+  const input = JSON.stringify({
+    actions: [
+      {
+        action: 'update',
+        module: 'avedep',
+        resolves: [
+          {
+            id: 123,
+            path: 'some-lib>another-lib>avedep',
+            dev: false,
+            optional: false,
+            bundled: false,
+          },
+        ],
+      },
+      {
+        action: 'install',
+        module: 'anotherdep',
+        resolves: [
+          {
+            id: 999,
+            path: 'some-lib>another-lib>anotherdep',
+            dev: false,
+            optional: false,
+            bundled: false,
+          },
+        ],
+      },
+      {
+        action: 'review',
+        module: 'baddep',
+        resolves: [
+          {
+            id: 456,
+            path: 'some-lib>another-lib>baddep',
+            dev: false,
+            optional: false,
+            bundled: false,
+          },
+        ],
+      },
+    ],
+    advisories,
+  });
+  const result = help(input);
+  t.equal(result.actions.length, 1);
+  t.equal(result.actions[0].resolves[0].id, 456);
   t.end();
 });
