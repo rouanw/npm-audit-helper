7396058a40a0d8788e8a5c25238cfbb1f25a8f12
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-09-18 09:22:26 +0200

    formatting

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const getStdin = require('get-stdin');
 const help = require('./lib/help');
 const report = require('./lib/report');
  
-getStdin().then(help).then(report).then(console.log).catch(console.error);
+getStdin()
+  .then(help)
+  .then(report)
+  .then(console.log)
+  .catch(console.error);
