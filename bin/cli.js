404383bef4c3ada56447b3ac46e86faf51d10b97
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-09-19 12:07:35 +0200

    exit with a sensible exit code

--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,14 @@
 const getStdin = require('get-stdin');
 const help = require('../lib/help');
 const report = require('../lib/report');
- 
+
 getStdin()
   .then(help)
-  .then(report)
-  .then(console.log)
+  .then((result) => {
+    return report(result)
+      .then((theReport) => {
+        console.log(theReport);
+        process.exit(result.exitCode);
+      });
+  })
   .catch(console.error);
