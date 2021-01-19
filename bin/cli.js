1a1697ccbd605cc04f87562c4487155453b725be
rouanw <rouan@carbontechlab.com>
2021-01-19 16:54:39 +0200

    branch on npm audit report version

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,9 @@
 
 const getStdin = require('get-stdin');
 const options = require('minimist')(process.argv.slice(2));
-const help = require('../lib/help');
+const npmAuditReportV2 = require('npm-audit-report-v2');
+const helpAuditV1 = require('../lib/help');
+const helpAuditV2 = require('../lib/help-audit-v2');
 const report = require('../lib/report');
 
 const defaults = {
@@ -16,9 +18,21 @@ const config = { ...defaults, ...options };
 (async function cli() {
   try {
     const stdin = await getStdin();
-    const auditResult = JSON.parse(stdin);
-    const result = help(config)(auditResult);
-    const theReport = await report(result);
+    const auditOutput = JSON.parse(stdin);
+    const newTreeFormat = auditOutput.auditReportVersion && auditOutput.auditReportVersion >= 2;
+    let reporter;
+    let helper;
+
+    if (newTreeFormat) {
+      reporter = (({ auditResult }) => console.log('\n*** NPM v7 is not supported yet, below is the raw output from `npm audit` ***\n') || npmAuditReportV2(auditResult).report);
+      helper = helpAuditV2;
+    } else {
+      reporter = report;
+      helper = helpAuditV1;
+    }
+
+    const result = helper(config)(auditOutput);
+    const theReport = await reporter(result);
     console.log(theReport);
     process.exit(result.exitCode);
   } catch (error) {
