9ef0419054d13ddd64103dcc90e9826205ab6388
rouanw <rouan@carbontechlab.com>
2021-02-04 11:22:06 +0200

    return the dependency with the most issues

--- test/test-audit-v2.js
+++ test/test-audit-v2.js
@@ -286,7 +286,146 @@ test('should return a count of auto fixes', (t) => {
   t.end();
 });
 
-test.skip('should return the most problematic dependency', (t) => {
+test('should return the most problematic dependency as the one with the most vulnerabilities', (t) => {
+  const input = buildAuditResultFixture({
+    handlebars: {
+      name: 'handlebars',
+      severity: 'high',
+      via: [
+        {
+          source: 61,
+          name: 'handlebars',
+          dependency: 'handlebars',
+          title: 'Cross-Site Scripting',
+          url: 'https://npmjs.com/advisories/61',
+          severity: 'high',
+          range: '<4.0.0',
+        },
+        {
+          source: 755,
+          name: 'handlebars',
+          dependency: 'handlebars',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/755',
+          severity: 'high',
+          range: '<=4.0.13 || >=4.1.0 <4.1.2',
+        },
+        'optimist',
+      ],
+      effects: [],
+      range: '<=4.7.3',
+      nodes: [
+        'node_modules/handlebars',
+      ],
+      fixAvailable: {
+        name: 'handlebars',
+        version: '4.7.6',
+        isSemVerMajor: true,
+      },
+    },
+    lodash: {
+      name: 'lodash',
+      severity: 'high',
+      via: [
+        {
+          source: 1065,
+          name: 'lodash',
+          dependency: 'lodash',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/1065',
+          severity: 'high',
+          range: '<4.17.12',
+        },
+        {
+          source: 1523,
+          name: 'lodash',
+          dependency: 'lodash',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/1523',
+          severity: 'low',
+          range: '<4.17.19',
+        },
+        {
+          source: 577,
+          name: 'lodash',
+          dependency: 'lodash',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/577',
+          severity: 'low',
+          range: '<4.17.5',
+        },
+        {
+          source: 782,
+          name: 'lodash',
+          dependency: 'lodash',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/782',
+          severity: 'high',
+          range: '<4.17.11',
+        },
+      ],
+      effects: [],
+      range: '<=4.17.18',
+      nodes: [
+        'node_modules/lodash',
+      ],
+      fixAvailable: {
+        name: 'lodash',
+        version: '4.17.20',
+        isSemVerMajor: true,
+      },
+    },
+    minimist: {
+      name: 'minimist',
+      severity: 'low',
+      via: [
+        {
+          source: 1179,
+          name: 'minimist',
+          dependency: 'minimist',
+          title: 'Prototype Pollution',
+          url: 'https://npmjs.com/advisories/1179',
+          severity: 'low',
+          range: '<0.2.1 || >=1.0.0 <1.2.3',
+        },
+      ],
+      effects: [
+        'optimist',
+      ],
+      range: '<0.2.1 || >=1.0.0 <1.2.3',
+      nodes: [
+        'node_modules/minimist',
+      ],
+      fixAvailable: {
+        name: 'handlebars',
+        version: '4.7.6',
+        isSemVerMajor: true,
+      },
+    },
+    optimist: {
+      name: 'optimist',
+      severity: 'low',
+      via: [
+        'minimist',
+      ],
+      effects: [
+        'handlebars',
+      ],
+      range: '>=0.6.0',
+      nodes: [
+        'node_modules/handlebars/node_modules/optimist',
+      ],
+      fixAvailable: {
+        name: 'handlebars',
+        version: '4.7.6',
+        isSemVerMajor: true,
+      },
+    },
+  });
+  const { mostProblematicDependency } = help(input);
+  t.equal(mostProblematicDependency.name, 'lodash');
+  t.equal(mostProblematicDependency.count, 4);
+  t.end();
 });
 
 test.skip('should not include actions for update or install when calculating the most problematic dependency', (t) => {
